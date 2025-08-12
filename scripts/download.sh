961453d3c224a727936a9e2785f62607db4aae3d
Igor Zaporozhets <zporozhets.igor@gmail.com>
2025-08-12 20:15:30 +0300

    build(download): download makefile script add

---
+++ scripts/download.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+MAKEFILE_URL="https://raw.githubusercontent.com/Zigr/fastapi_debug_toolkit/refs/heads/master/scripts/Makefile.fastapi_debug_toolkit"
+CONFIG_URL="https://raw.githubusercontent.com/Zigr/fastapi_debug_toolkit/refs/heads/master/scripts/fastapi_debug_toolkit.config.mk"
+
+curl -LO  "MAKEFILE_URL" -LO "CONFIG_URL"
